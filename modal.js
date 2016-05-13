2eeee4fdeda9a5277641acf4c846b23d9b5be0d8
Blake Newman <blake.newman@sainsburys.co.uk>
2016-05-13 14:24:40 +0100

    added modal component

--- modal.js
+++ modal.js
@@ -1,5 +1,16 @@
 Vue.component('modal', {
-
+  template: '#modal-template',
+  data: function() {
+    return { show: false };
+  },
+  methods: {
+    open: function() {
+      this.show = true;
+    },
+    close: function() {
+      this.show = false;
+    }
+  }
 });
 
 // start app
